0a00433311c364a7cbcb44faddc80082eb3d1c0e
henrique ameixa <henrique-ameixa@hotmail.com>
2025-02-13 11:44:39 -0300

    Add initial project setup with Next.js, including configuration files, basic structure, and Azure deployment workflow

---
+++ README.md
@@ -0,0 +1,3 @@
+# StackDiario
+Um blog Stack Diário
+morreu 1 vez
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Bem-vindo ao Next.js!</h1>
+      </div>
+    );
+  }
